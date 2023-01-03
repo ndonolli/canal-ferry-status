dc3143a27ee7a8166e2bc426a1abb180a5dd9d9c
ndonolli <ndonolli@gmail.com>
2023-01-03 00:46:48 -0600

    test api with hello world

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,25 +3,32 @@ const twitter = require('twitter-api-sdk');
 
 const API_ENDPOINT = 'https://api.twitter.com/2/users/43169410/tweets';
 
-exports.handler = async (event, context) => {
-  let response;
-  const client = new twitter.Client(process.env.BEARER_TOKEN);
-  try {
-    const response = await client.tweets.usersIdTweets('43169410');
-    // handle response
-  } catch (err) {
-    return {
-      statusCode: err.statusCode || 500,
-      body: JSON.stringify({
-        error: err.message,
-      }),
-    };
-  }
+// exports.handler = async (event, context) => {
+//   let response;
+//   const client = new twitter.Client(process.env.BEARER_TOKEN);
+//   try {
+//     const response = await client.tweets.usersIdTweets('43169410');
+//     // handle response
+//   } catch (err) {
+//     return {
+//       statusCode: err.statusCode || 500,
+//       body: JSON.stringify({
+//         error: err.message,
+//       }),
+//     };
+//   }
 
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       data: response,
+//     }),
+//   };
+// };
+
+exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      data: response,
-    }),
+    body: JSON.stringify({ message: 'Hello World' }),
   };
 };
