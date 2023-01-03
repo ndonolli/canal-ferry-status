88a59e3a0f1e87b9ea015b458e7ec9cd5a353237
ndonolli <ndonolli@gmail.com>
2023-01-03 00:43:28 -0600

    test call from frontend

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 // test
+fetch('https://canal-ferry-status.netlify.app/.netlify/functions/api')
+  .then(console.log)
+  .catch(console.log);
