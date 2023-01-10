7560d38a1d4c4e2008dd16c9f7bed0a2aa1e841b
ndonolli <ndonolli@gmail.com>
2023-01-09 18:53:40 -0600

    wrap response object

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -21,6 +21,8 @@ exports.handler = async (event, context) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify(response),
+    body: JSON.stringify({
+      data: response,
+    }),
   };
 };
